d952378b1956be6ae45bce3f0823b1b2b86549f0
vic-algonquin <pham0149@algonquinlive.com>
2020-11-01 18:08:11 -0500

    Improve eat() in Cat class, fix bug in Mouse class

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
 var dog = new Dog('Tom');
 var tom = new Cat(); 
+var jerry = new Mouse('Jerry'); 
+
 dog.sayHi();
 dog.eat(tom);
 console.log(dog);
+
+tom.eat(jerry);
+console.log(tom);
+
+try {
+    tom.eat(dog);
+} catch (error) {
+    console.log('Cat cannnot eat dog');
+}
